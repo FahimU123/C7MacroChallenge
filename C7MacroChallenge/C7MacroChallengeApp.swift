0442155acda8446852b865f29e76185e2aeef6ef
FahimU313 <115724162+FahimU313@users.noreply.github.com>
2025-04-28 14:59:44 -0400

    Initial Commit

---
+++ C7MacroChallenge/C7MacroChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  C7MacroChallengeApp.swift
+//  C7MacroChallenge
+//
+//  Created by Fahim Uddin on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct C7MacroChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
