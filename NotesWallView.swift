7b436c0f3a541cbe0ba7fb1a84ee90c4b400205b
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-05-01 13:50:36 -0400

    NoteWallView/CardView, and more

---
+++ NotesWallView.swift
@@ -0,0 +1,18 @@
+//
+//  MainWhyView.swift
+//  C7MacroChallenge
+//
+//  Created by Davaughn Williams on 4/29/25.
+//
+
+import SwiftUI
+
+struct WallView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WallView()
+}
