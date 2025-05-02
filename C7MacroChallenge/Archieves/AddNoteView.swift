7975ef428e6831061ffdb9bdb09e0c6bb6d0458d
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-05-02 16:50:29 -0400

    😳

---
+++ C7MacroChallenge/Archieves/AddNoteView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNoteView.swift
+//  C7MacroChallenge
+//
+//  Created by Davaughn Williams on 5/2/25.
+//
+
+import SwiftUI
+
+struct AddNoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNoteView()
+}
---
+++ C7MacroChallenge/Archieves/NoteData.swift
@@ -0,0 +1,8 @@
+//
+//  NoteData.swift
+//  C7MacroChallenge
+//
+//  Created by Davaughn Williams on 5/2/25.
+//
+
+import Foundation
